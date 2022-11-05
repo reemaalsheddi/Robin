64d3b5338d2395debb45cad91b962a8140ae8681
ReemAlk <r.yk99@hotmail.com>
2022-11-05 15:59:01 +0300

    First commit

---
+++ model/RobinViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RobinViewModel.swift
+//  Robin
+//
+//  Created by Reem Alkhatib on 31/10/2022.
+//
+
+import SwiftUI;
+import CoreData
+    class RobinViewModel: ObservableObject {
+    @Published var currentTab: String = "Today"
+    }
